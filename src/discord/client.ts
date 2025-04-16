fd0c207ca06754a27349b14e9d7f935610c89e4f
saltcute <me@mizuno.ai>
2025-04-16 05:39:28 -0400

    fix: chuni not using database

--- src/discord/client.ts
+++ src/discord/client.ts
@@ -9,6 +9,11 @@ MaiDraw.Maimai.Chart.Database.setLocalDatabasePath(
         ? kasumi.config.getSync("maimai::config.localDatabasePath")
         : ""
 );
+MaiDraw.Chuni.Chart.Database.setLocalDatabasePath(
+    kasumi.config.getSync("maimai::config.useLocalDatabase")
+        ? kasumi.config.getSync("maimai::config.localDatabasePath")
+        : ""
+);
 
 client.on("ready", () => {
     kasumi.logger.info(`Logged in as ${client.user?.tag}!`);
